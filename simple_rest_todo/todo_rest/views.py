98a0a200ef36a978db2becb65b6d92bdd30c99db
Vasil <vrangelov85@abv.bg>
2023-07-22 10:16:15 +0300

    Fixed previous commit message - its "O" from SOLID, not "S"

--- simple_rest_todo/todo_rest/views.py
+++ simple_rest_todo/todo_rest/views.py
@@ -3,36 +3,36 @@ from .models import TaskToDo
 from .serializers import TaskSerializer
 
 
-class TaskCreateView(
-    generics.CreateAPIView
-):  # "Create" operation (C) - It handles the POST request !
+class BasicTaskView(generics.GenericAPIView):
     queryset = TaskToDo.objects.all()
     serializer_class = TaskSerializer
+
+
+class TaskCreateView(
+    BasicTaskView, generics.CreateAPIView
+):  # "Create" operation (C) - It handles the POST request !
+    pass
 
 
 class TaskListView(
-    generics.ListAPIView
+    BasicTaskView, generics.ListAPIView
 ):  # Read operation (R) - It handles the GET request !
-    queryset = TaskToDo.objects.all()
-    serializer_class = TaskSerializer
+    pass
 
 
 class TaskUpdateView(
-    generics.UpdateAPIView
+    BasicTaskView, generics.UpdateAPIView
 ):  # "Update" operation (U) - It handles the PUT and PATCH requests !
-    queryset = TaskToDo.objects.all()
-    serializer_class = TaskSerializer
+    pass
 
 
 class TaskDestroyView(
-    generics.DestroyAPIView
+    BasicTaskView, generics.DestroyAPIView
 ):  # "Delete/Destroy" operation (D) -  It handles the DELETE request !
-    queryset = TaskToDo.objects.all()
-    serializer_class = TaskSerializer
+    pass
 
 
 class TaskRetrieveView(
     generics.RetrieveAPIView
 ):  # "Read" operation (R) for a SINGLE ENTITY - handles GET again !!!
-    queryset = TaskToDo.objects.all()
-    serializer_class = TaskSerializer
+    pass
